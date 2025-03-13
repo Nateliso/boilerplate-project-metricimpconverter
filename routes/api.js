f90c9baae11ddc210099aeba3f42d339841a6012
Nateliso <mhlanaliso18@gmail.com>
2025-03-13 08:52:46 +0200

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -7,4 +7,32 @@ module.exports = function (app) {
   
   let convertHandler = new ConvertHandler();
 
+  app.get('/api/convert', (req, res) => {
+    let input = req.query.input;
+
+    let initNum = convertHandler.getNum(input);
+    let initUnit = convertHandler.getUnit(input);
+
+    if (initNum === null && initUnit === null) {
+        return res.json({ error: "invalid number and unit" });
+    }
+    if (initNum === null) {
+        return res.json({ error: "invalid number" });
+    }
+    if (initUnit === null) {
+        return res.json({ error: "invalid unit" });
+    }
+
+    let returnUnit = convertHandler.getReturnUnit(initUnit);
+    let returnNum = convertHandler.convert(initNum, initUnit);
+    let string = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+
+    res.json({
+        initNum,
+        initUnit,
+        returnNum,
+        returnUnit,
+        string
+    });
+  });
 };
